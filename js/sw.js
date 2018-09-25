1c7af0858b55a32f68168ab2ffa2aff0d7cfde11
Kedar Bhusal <kedarbhusal@yahoo.com>
2018-09-25 23:48:51 +0545

    feat: Add service worker

---
+++ js/sw.js
@@ -0,0 +1,48 @@
+let cacheName = 'cache-v1';
+
+//list of all filea to be chached
+const assets = ['/',
+				'/index.html',
+				'/restaurant.html',
+				'/js/dbhelper.js',
+				'/js/main.js',
+				'/js/restaurant_info.js',
+				'/data/restaurants.json',
+				'/css/style.css',
+				'/css/styles.css',
+				'/js/user.js',
+				'/img/1.jpg',
+				'/img/2.jpg',
+				'/img/3.jpg',
+				'/img/4.jpg',
+				'/img/5.jpg',
+				'/img/6.jpg',
+				'/img/7.jpg',
+				'/img/8.jpg',
+				'/img/9.jpg',
+				'/img/logo.svg',
+				'/img/logo-white.svg',
+				'/img/sprite.svg'
+			];
+
+self.addEventListener('install', function (event) {
+	event.waitUntil(caches.open(cacheName).then(function (cache) {
+		return cache.addAll(assets).catch(function (error) {
+			console.log("Error: ", error);
+		});
+	}));
+});
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			if (response) {
+				console.log('found it', event.request, 'in cache');
+				return response;
+			} else {
+				cosole.log('not found')
+				return fetch(event.request);
+			}
+		})
+	);
+});
--- js/user.js
+++ js/user.js
@@ -2,6 +2,14 @@
 openNav = () => {
   document.getElementById('sidenav').style.width = "25rem";
 }
+//close navigation menu
 closeNav = () => {
   document.getElementById('sidenav').style.width = "0";
 }
+
+//register service worker
+navigator.serviceWorker.register('/js/sw.js').then(function(reg) {
+	console.log("Service worker is registered.");
+}).catch(function(err){
+	console.log("Error:", err);
+});
