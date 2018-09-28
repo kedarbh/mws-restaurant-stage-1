691f48fe5a1e52ab7ffce351fe287f8d5a646b77
kedarbh <32976186+kedarbh@users.noreply.github.com>
2018-09-28 12:27:47 +0545

    Update user.js

--- js/user.js
+++ js/user.js
@@ -8,7 +8,7 @@ closeNav = () => {
 }
 
 //register service worker
-navigator.serviceWorker.register('./service-worker.js', {scope: '/'}).then(function(reg) {
+navigator.serviceWorker.register('./service-worker.js').then(function(reg) {
 	console.log("Service worker is registered", reg.scope);
 }).catch(function(err){
 	console.log("Service Worker failed to register. ", err);
